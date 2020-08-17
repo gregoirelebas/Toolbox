fbba042fb0efa9e28bfe03676d789a3391aecbf9
Grégoire Lebas <gregoire.lebas@gmail.com>
2020-08-17 14:54:07 +0200

    Created 2 types of Singleton.
    Persistent : Stay between scenes. Temporary : Is destroyed at scene unloading. Both are created by Ressources.Load().

---
+++ Assets/Scripts/PersistantSingleton.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersistantSingleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+	private static T instance = null;
+	public static T Instance
+	{
+		get
+		{
+			if (instance == null)
+			{
+				SetInstance();
+			}
+
+			return instance;
+		}
+	}
+
+	private static void SetInstance(T current = null)
+	{
+		if (current == null)
+		{
+			GameObject go = Resources.Load<GameObject>(typeof(T).ToString() + ".prefab");
+			instance = go.GetComponent<T>();
+
+			DontDestroyOnLoad(go);
+		}
+		else
+		{
+			instance = current;
+
+			DontDestroyOnLoad(current.gameObject);
+		}
+	}
+
+	protected virtual void Awake()
+	{
+		if (instance == null)
+		{
+			SetInstance(GetComponent<T>());
+		}
+		else if (instance != this)
+		{
+			Destroy(gameObject);
+			return;
+		}
+	}
+}
---
+++ Assets/Scripts/TemporarySingleton.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TemporarySingleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+	private static T instance = null;
+	public static T Instance
+	{
+		get
+		{
+			if (instance == null)
+			{
+				SetInstance();
+			}
+
+			return instance;
+		}
+	}
+
+	private static void SetInstance(T current = null)
+	{
+		if (current == null)
+		{
+			GameObject go = Resources.Load<GameObject>(typeof(T).ToString() + ".prefab");
+			instance = go.GetComponent<T>();
+		}
+		else
+		{
+			instance = current;
+		}
+	}
+
+	protected virtual void Awake()
+	{
+		if (instance == null)
+		{
+			SetInstance(GetComponent<T>());
+		}
+		else if (instance != this)
+		{
+			Destroy(gameObject);
+			return;
+		}
+	}
+}
