b04a8b926855a0c05818fbbadf19446064923dbc
Grégoire Lebas <gregoire.lebas@gmail.com>
2020-09-02 15:16:06 +0200

    Created Extensions of Monobehaviour.
    Add some useful functions to Monobehaviour as an extension.

---
+++ Assets/Scripts/Extensions/MonoBehaviour_Extend.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class MonoBehaviour_Extend
+{
+	/// <summary>
+	/// Invoke [action] after [delay] seconds.
+	/// </summary>
+	public static void DelayedAction(this MonoBehaviour runner, Action action, float delay, bool unscaledTime = false)
+	{
+		runner.StartCoroutine(DelayedActionRoutine(action, delay, unscaledTime));
+	}
+
+	private static IEnumerator DelayedActionRoutine(Action action, float delay, bool unscaledTime = false)
+	{
+		if (unscaledTime)
+		{
+			yield return new WaitForSecondsRealtime(delay);
+		}
+		else
+		{
+			yield return new WaitForSeconds(delay);
+		}
+
+		action?.Invoke();
+	}
+
+	/// <summary>
+	/// Disable the GameObject after [delay] seconds.
+	/// </summary>
+	public static void DisableAfterDelay(this MonoBehaviour runner, float delay, bool unscaledTime = false)
+	{
+		runner.StartCoroutine(DelayedActionRoutine(() =>
+		{
+			runner.gameObject.SetActive(false);
+		}, delay, unscaledTime));
+	}
+}
