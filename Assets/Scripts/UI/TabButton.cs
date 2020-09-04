ff485d76920201094d8741721273e36a31f9220f
Grégoire Lebas <gregoire.lebas@gmail.com>
2020-09-04 15:20:03 +0200

    Created a tab system.

---
+++ Assets/Scripts/UI/TabButton.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace Toolbox
+{
+	[RequireComponent(typeof(Image))]
+	public class TabButton : MonoBehaviour, IPointerEnterHandler, IPointerClickHandler, IPointerExitHandler
+	{
+		[HideInInspector] public Image background = null;
+		
+		[SerializeField] private TabGroup group = null;
+		[SerializeField] private GameObject panel = null;
+
+		[SerializeField] private UnityEvent onTabSelected = null;
+		[SerializeField] private UnityEvent onTabDeselected = null;
+		
+		private void Awake()
+		{
+			background = GetComponent<Image>();
+		}
+
+		private void Start()
+		{
+			group.Subscribe(this);
+		}
+
+		public void OnPointerEnter(PointerEventData eventData)
+		{
+			group.OnTabEnter(this);
+		}
+		
+		public void OnPointerClick(PointerEventData eventData)
+		{
+			group.OnTabSelected(this);
+		}
+
+		public void OnPointerExit(PointerEventData eventData)
+		{
+			group.OnTabExit(this);
+		}
+
+		public void SetPanelActive(bool active)
+		{
+			panel.SetActive(active);
+		}
+
+		public void Select()
+		{
+			onTabSelected?.Invoke();
+		}
+
+		public void Deselect()
+		{
+			onTabDeselected?.Invoke();
+		}
+	}
+}
---
+++ Assets/Scripts/UI/TabGroup.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Toolbox
+{
+	public class TabGroup : MonoBehaviour
+	{
+		[Header("State sprites")]
+		[SerializeField] private Sprite idleTabSprite = null;
+		[SerializeField] private Sprite hoverTabSprite = null;
+		[SerializeField] private Sprite activeTabSprite = null;
+
+		private List<TabButton> buttons = null;
+		private TabButton selected = null;
+
+		public void Subscribe(TabButton button)
+		{
+			if (buttons == null)
+			{
+				buttons = new List<TabButton>();
+			}
+
+			buttons.Add(button);
+		}
+
+		public void OnTabEnter(TabButton button)
+		{
+			ResetTabs();
+
+			if (!button.Equals(selected))
+			{
+				button.background.sprite = hoverTabSprite;
+			}
+		}
+
+		public void OnTabSelected(TabButton button)
+		{
+			selected?.Deselect();
+			selected = button;
+			selected?.Select();
+
+			ResetTabs();
+
+			button.background.sprite = activeTabSprite;
+
+			foreach (TabButton currentButton in buttons)
+			{
+				currentButton.SetPanelActive(false);
+			}
+
+			button.SetPanelActive(true);
+		}
+
+		public void OnTabExit(TabButton button)
+		{
+			ResetTabs();
+		}
+
+		public void ResetTabs()
+		{
+			foreach (TabButton button in buttons)
+			{
+				if (!button.Equals(selected))
+				{
+					button.background.sprite = idleTabSprite;
+				}
+			}
+		}
+	}
+}
