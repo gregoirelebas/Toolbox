be16c19b01c2359b09efe243ca77571532641212
Grégoire Lebas <gregoire.lebas@gmail.com>
2020-09-04 12:49:04 +0200

    Created a BlurPanel component.
    Blur everything behind it and fade in.

---
+++ Assets/Editor/BlurPanelEditor.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.UI;
+using UnityEngine;
+
+[CustomEditor(typeof(BlurPanel))]
+public class BlurPanelEditor : ImageEditor
+{
+	private SerializedProperty isAnimated;
+
+	public override void OnInspectorGUI()
+	{
+		base.OnInspectorGUI();
+
+
+		EditorGUI.BeginChangeCheck();
+
+		EditorGUILayout.PropertyField(serializedObject.FindProperty("blurValue"));
+		EditorGUILayout.PropertyField(serializedObject.FindProperty("delay"));
+
+		isAnimated = serializedObject.FindProperty("isAnimated");
+		EditorGUILayout.PropertyField(isAnimated);
+		if (isAnimated.boolValue)
+		{
+			EditorGUILayout.PropertyField(serializedObject.FindProperty("time"));
+		}
+
+		if (EditorGUI.EndChangeCheck())
+		{
+			serializedObject.ApplyModifiedProperties();
+		}
+	}
+}
--- Assets/Scripts/Extensions/MonoBehaviour_Extend.cs
+++ Assets/Scripts/Extensions/MonoBehaviour_Extend.cs
@@ -5,14 +5,6 @@ using UnityEngine;
 
 public static class MonoBehaviour_Extend
 {
-	/// <summary>
-	/// Invoke [action] after [delay] seconds.
-	/// </summary>
-	public static void DelayedAction(this MonoBehaviour runner, Action action, float delay, bool unscaledTime = false)
-	{
-		runner.StartCoroutine(DelayedActionRoutine(action, delay, unscaledTime));
-	}
-
 	private static IEnumerator DelayedActionRoutine(Action action, float delay, bool unscaledTime = false)
 	{
 		if (unscaledTime)
@@ -28,6 +20,14 @@ public static class MonoBehaviour_Extend
 	}
 
 	/// <summary>
+	/// Invoke [action] after [delay] seconds.
+	/// </summary>
+	public static void DelayedAction(this MonoBehaviour runner, Action action, float delay, bool unscaledTime = false)
+	{
+		runner.StartCoroutine(DelayedActionRoutine(action, delay, unscaledTime));
+	}
+
+	/// <summary>
 	/// Disable the GameObject after [delay] seconds.
 	/// </summary>
 	public static void DisableAfterDelay(this MonoBehaviour runner, float delay, bool unscaledTime = false)
@@ -36,5 +36,23 @@ public static class MonoBehaviour_Extend
 		{
 			runner.gameObject.SetActive(false);
 		}, delay, unscaledTime));
+	}
+
+	private static IEnumerator DoActionOnTickRoutine(Action action)
+	{
+		while (true)
+		{
+			action?.Invoke();
+
+			yield return null;
+		}
+	}
+
+	/// <summary>
+	/// Infinite call of an action every frame. Use StopCoroutine to stop calling.
+	/// </summary>
+	public static Coroutine DoActionOnTick(this MonoBehaviour runner, Action action)
+	{
+		return runner.StartCoroutine(DoActionOnTickRoutine(action));
 	}
 }
