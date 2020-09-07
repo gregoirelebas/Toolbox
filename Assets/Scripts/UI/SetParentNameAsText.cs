d80dbea61215b103149ee7a4a930f27e8bf82847
Grégoire Lebas <gregoire.lebas@gmail.com>
2020-09-07 14:37:31 +0200

    Created script to set name of parent as text.

---
+++ Assets/Scripts/UI/SetParentNameAsText.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Toolbox
+{
+	public class SetParentNameAsText : MonoBehaviour
+	{
+		[SerializeField] private bool useTMP = true;
+
+		private void Awake()
+		{
+			if (useTMP)
+			{
+				GetComponent<TextMeshProUGUI>().text = transform.parent.name;
+			}
+			else
+			{
+				GetComponent<Text>().text = transform.parent.name;
+			}
+		}
+	}
+}
